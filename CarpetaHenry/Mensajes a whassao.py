a1575bb578a68462d9df1f3d0a1e13381a48ae06
ricardo567 <ricardo.holguin567@gmail.com>
2023-08-15 09:08:02 -0400

    Mensaje de WhatsApp

--- CarpetaHenry/Mensajes a whassao.py
+++ CarpetaHenry/Mensajes a whassao.py
@@ -2,13 +2,14 @@ import pyautogui, webbrowser
 from time import sleep
  
 
-webbrowser.open('http-equiv="X-UA-Compatible"')
+webbrowser.open('https://web.whatsapp.com/send?phone=+Numero de la persona')
 
 sleep(5)
 
-for i in range(100):
-    pyautogui.typewrite('Good people in the world')
+for i in range(101):
+    pyautogui.typewrite('Aqui puedes escribir mensajes aleatorios.')
     pyautogui.press('enter')
     
   
+
   
